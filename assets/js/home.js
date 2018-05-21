bb513a1c06f7f67d610eaad82dfe1e27020af9b6
Jimmy <JimmyCao227@yahoo.com>
2018-05-21 00:54:40 -0400

    Added functionality to buttons (reset, resize) and some changes to the look of the webpage.

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,6 @@
 let grids = 16;
-let color = 'gray';
+let color = 'pink';
+let opacity = 0;
 const container = document.querySelector('#container');
 
 function createGrid(){
@@ -15,6 +16,19 @@ function createGrid(){
 			})
 		}
 	}
+}
+
+function reset(){
+	while(container.firstChild){
+		container.removeChild(container.firstChild);
+	}
+	createGrid();
+}
+
+function resize(){
+	grids = prompt("Enter a number for the squares per side.");
+	document.body.style.setProperty('--columnNumber', grids);
+	reset();
 }
 	
 createGrid();
